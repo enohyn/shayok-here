92595b662ff8f2e48cbc79e65c4128a9ea7e9f83
shayok-viva <shayok.ullah@vivasoftltd.com>
2025-08-18 00:11:14 +0600

    Footer added

---
+++ components/Footer/site-footer.tsx
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import React from 'react';
+import { AiOutlineLinkedin } from "react-icons/ai";
+import { VscGithub } from 'react-icons/vsc';
+function SiteFooter() {
+  return (
+    <footer className='bg-gradient-to-tr from-zinc-900 via-zinc-900 h-full border-t-l bottom-0 flex items-center '>
+      <nav className='py-4 w-full container mx-auto'>
+        <div className='flex justify-between items-center'>
+          <a href='#meet-shayok' className='list-none capitalize flex items-center justify-start w-1/3'>
+              <span className='font-megrim text-lg lg:text-lg font-extrabold text-white hover:text-teal-600 transition'>&copy; Shayok</span>
+          </a>
+          
+          <div className='flex gap-x-4 w-2/3 lg:w-1/3 justify-end max-lg:items-center '>
+            <span className='font-space text-base lg:text-lg font-semibold text-white'>Look me up:</span>
+            <Link href={`https://github.com/enohyn`} target='_blank'>
+              <VscGithub className='lg:size-6 text-white hover:text-teal-600 transition' />
+            </Link>
+            <Link href={`https://www.linkedin.com/in/shayok-here/`} target='_blank'>
+              <AiOutlineLinkedin className='lg:size-6 text-white hover:text-teal-600 transition' />
+            </Link>
+          </div>
+
+        </div>
+      </nav>
+    </footer>
+  )
+}
+
+export default SiteFooter;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import SiteHeader from "../../components/Header/site-header";
+import SiteFooter from "../../components/Footer/site-footer";
 
 export default function Home() {
   return (
     <div className="bg-fixed bg-gradient-to-tr from-zinc-900 via-zinc-900 to-teal-900 w-screen">
       <SiteHeader />
+      <SiteFooter />
     </div>
   );
 }
