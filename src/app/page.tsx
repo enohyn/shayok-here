e1e28fbd46c4f248bea4b9a75df91f7457b554bc
shayok-viva <shayok.ullah@vivasoftltd.com>
2025-08-28 17:58:18 +0600

    Hero section added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Image from "next/image";
 import SiteHeader from "../../components/Header/site-header";
 import SiteFooter from "../../components/Footer/site-footer";
+import HeroSection from "../../components/Hero/hero-section";
 
 export default function Home() {
   return (
     <div className="bg-fixed bg-gradient-to-tr from-zinc-900 via-zinc-900 to-teal-900 w-screen">
       <SiteHeader />
+      <HeroSection />
       <SiteFooter />
     </div>
   );
