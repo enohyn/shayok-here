25b631c37021c4e20bbe23f977218939b991ea8c
shayok-viva <shayok.ullah@vivasoftltd.com>
2025-08-28 17:57:58 +0600

    menu added

---
+++ constants/menu.ts
@@ -0,0 +1,14 @@
+export const MENU_PATHS = [
+    {
+        path: "#about",
+        title: "About"
+    },
+    {
+        path: "#portfolio",
+        title: "Portfolio"
+    },
+    {
+        path: "/sign-in",
+        title: "Sign In"
+    },
+]
